031667e133c877d6a17b4523b878dfeee9dbd41b
Zamfir Tucudeanu <zamfir.tucudeanu@gmail.com>
2020-07-29 02:36:10 +0300

    updated with html code

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import os
 import subprocess
 
-app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
-def webroot():
-
-    # generate message
-    Welcome = "Hello, Miki! Below data is for you!"
-    OS_python = "This code is running: " + subprocess.check_output(["python", "--version"], universal_newlines=True)
-    OS_name =  "Hostname: " + subprocess.check_output(["hostname"], universal_newlines=True)
-    OS_kernel = "Kernel version: " + subprocess.check_output(["uname", "-r"], universal_newlines=True)
-    message = Welcome + OS_name + OS_kernel + OS_python
+def generate_info_html():
+    html_frame = """
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Info Page</title>
+    </head>
+    <body>
+        <h1>We are running on:</h1>
+        <p><b>Hostname:</b> %s</p>
+        <p><b>Kernel version:</b> %s</p>
+        <p><b>Python version:</b> %s</p>
+        <b><p>Click <a href="/">here</a> to go home.</p></b>
+    </body>
+</html>
+"""
+    osname =  subprocess.check_output(["hostname"], universal_newlines=False)
+    oskernel = subprocess.check_output(["uname", "-r"], universal_newlines=False)
+    ospython = subprocess.check_output(["python", "--version"], universal_newlines=False)
+    f = open("static/info.html", "w")
+    whole = html_frame % (osname, oskernel, ospython)
+    f.write(whole)
+    f.close()
     
-    # display it on webpage
-    return message #app.send_static_file('./index.html')
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+@app.route('/info', methods=['GET', 'POST'])
+def info():
+    generate_info_html()
+    return app.send_static_file('info.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='127.0.0.1', port=int(os.environ.get('PORT', 5000)))
