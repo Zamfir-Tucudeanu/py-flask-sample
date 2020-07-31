710cc0f7b375c0f88d7eb85fa64a42fe5a88b945
Zamfir Tucudeanu <zamfir.tucudeanu@gmail.com>
2020-08-01 02:16:29 +0300

    added fs for api

--- app.py
+++ app.py
@@ -9,13 +9,24 @@ def get_net():
     #print(net_info)
     return dict(json.loads((net_info)))
 
+def get_fs():
+    # mountpoints:
+    mounts = str(subprocess.getoutput('cat /proc/mounts'))
+    fsdict ={}
+    for sfs in mounts.split('\n'):
+        fs = sfs.split()
+        fsdict.update({fs[1]: dict(fsdevice=fs[0], fstype=fs[2], fsmountargs=fs[3], fsdump=fs[4], fsccheckorder=fs[5])})
+    #print(fsdict)
+    return fsdict
+
 def get_cpu():
     # cpu:
     cpu_vendor = str(subprocess.getoutput('cat /proc/cpuinfo | grep "vendor_id" | uniq').split(":")[1])
     cpu_model = str(subprocess.getoutput('cat /proc/cpuinfo | grep "model name" | uniq').split(":")[1])
     cpu_cores = str(subprocess.getoutput('cat /proc/cpuinfo | grep "cpu cores" | uniq').split(":")[1])
 
-    cpudict = dict(cpuvendor=cpu_vendor, cpumodel=cpu_model, cpucores=cpu_cores)
+    cpudict = {}
+    cpudict.update(dict(cpuvendor=cpu_vendor, cpumodel=cpu_model, cpucores=cpu_cores))
     #print(json.dumps(cpudict))
     #return json.dumps(cpudict)
     return cpudict
@@ -27,7 +38,8 @@ def get_mem():
     swap_total = str(subprocess.getoutput('cat /proc/meminfo | grep "SwapTotal" | uniq | tr -s [:blank:]').split(":")[1])
     swap_free = str(subprocess.getoutput('cat /proc/meminfo | grep "SwapFree" | uniq | tr -s [:blank:]').split(":")[1])
 
-    memdict = dict(memorytotal=mem_total, memfree=mem_free, swaptotal=swap_total, swapfree=swap_free)
+    memdict = {}
+    memdict.update(dict(memorytotal=mem_total, memfree=mem_free, swaptotal=swap_total, swapfree=swap_free))
     #print(json.dumps(memdict))
     #return json.dumps(memdict)
     return memdict
@@ -40,7 +52,8 @@ def get_os():
     os_proc = str(subprocess.getoutput('uname -p'))
     os_node = str(subprocess.getoutput('uname -n'))
 
-    osdict = dict(os=os_os, kernel=os_kernel, hw=os_hw, proc=os_proc, node=os_node)
+    osdict = {}
+    osdict.update(dict(os=os_os, kernel=os_kernel, hw=os_hw, proc=os_proc, node=os_node))
     #return json.dumps(osdict)
     return osdict
 
@@ -52,16 +65,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    supported = "os|OS|mem|MEM|net|NET|cpu|CPU"
+    #supported = "os|OS|mem|MEM|net|NET|cpu|CPU"
+    supported = "os|OS|mem|MEM|net|NET|cpu|CPU|fs|FS"
     return default(supported)
 
 @app.route('/<string:req>', methods=['GET', 'POST'])
 def get_info(req):
-    supported = "os|OS|mem|MEM|net|NET|cpu|CPU"
+    supported = "os|OS|mem|MEM|net|NET|cpu|CPU|fs|FS"
     if req.lower() == 'cpu': return get_cpu()
     if req.lower() == 'mem': return get_mem()
     if req.lower() == 'net': return get_net()
     if req.lower() == 'os': return get_os()
+    if req.lower() == 'fs': return get_fs()
     if req not in supported: return default(supported)
 
 
--- testapi.py
+++ testapi.py
@@ -1,13 +1,18 @@
 import requests
 
 baseurl = "http://127.0.0.1:5000/"
+
+# get
 response = requests.get(baseurl + "os")
 print('+++++++++++++')
 print("method == GET")
 print('++++++++++++++++++++++++++++++++++++++')
 print(response.json())
 print('++++++++++++++++++++++++++++++++++++++')
+
 print("")
+
+# post
 response = requests.post(baseurl + "mem")
 print('++++++++++++++')
 print("method == POST")
